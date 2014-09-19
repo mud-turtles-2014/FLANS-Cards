5c7561672c0e2f5c6e00d052054fe8dbaffcca17
Alex Ung <alexung.ucsb@gmail.com>
2014-09-19 16:06:32 -0400

    Create index controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,48 @@
 enable :sessions
 
+before '/:username/home' do
+  @user = User.find_by(username: params[:username])
+  session[:user_id] == @user.id ? @valid = true : @valid = false
+end
+
 get "/" do
   erb :index
 end
+
+get "/session/new" do
+  erb :'users/form'
+end
+
+post "/session" do
+  user = User.authenticate(params[:username], params[:password])
+
+  if user == nil
+    redirect "/session/new"
+  end
+
+  unless user == nil
+    session[:user_id] = user.id
+    redirect "/#{user.username}/home"
+  end
+end
+
+post "/user/create" do
+  user = User.new(params[:user])
+  user.save
+  redirect "/session/new"
+end
+
+get "/user/new" do
+  erb :'users/create_user'
+end
+
+get '/:username/home' do
+  redirect '/session/new' if @valid == false
+  erb :'users/landing_page'
+end
+
+delete '/session' do
+  session.clear
+  erb :logout
+end
+
